78772d048a49db71a902e91f9c2f6cddb41aa137
enexer <as94lol@gmail.com>
2019-07-20 02:32:49 +0200

    init

---
+++ src/main/scala/init/InitSpark.scala
@@ -0,0 +1,23 @@
+package init
+
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.{SparkConf, SparkContext}
+
+trait InitSpark extends App{
+  // INFO DISABLED
+  Logger.getLogger("org").setLevel(Level.OFF)
+  Logger.getLogger("akka").setLevel(Level.OFF)
+  Logger.getLogger("INFO").setLevel(Level.OFF)
+
+  val conf: SparkConf = new SparkConf()
+    .setAppName("testApp")
+    .setMaster("local")
+
+
+  val sc: SparkContext = new SparkContext(conf)
+  val spark: SparkSession = SparkSession.builder().config(conf).getOrCreate()
+
+
+  def printX(s: Any) = println("=========>"+s)
+}
