6c440010a6585e613c43d5ba4c49aabc9b578dd6
andrzej.szewczyk <andrzej.szewczyk@accenture.com>
2019-08-01 11:33:10 +0200

    add task3 - streaming

---
+++ src/main/scala/tasks2/Task3.scala
@@ -0,0 +1,55 @@
+package tasks2
+
+import init.InitSpark
+
+object Task3 extends InitSpark {
+
+
+
+  import spark.implicits._
+  import org.apache.spark.sql.functions._
+  spark.readStream
+    .format("socket")
+    .option("host", "localhost")
+    .option("port", 9999)
+    .load()
+    .agg(collect_list('value).as("list")) // make aggregation for complete output
+    .select(explode('list)) // explode list to show all values
+    .writeStream
+    .format("console")
+    .outputMode("complete") // need aggregation
+    .option("checkpointLocation", "data/checkpoint")
+    .start()
+    .awaitTermination()
+
+  //Complete output mode not supported when there are no streaming aggregations on streaming DataFrames/Datasets;;
+  //Append output mode not supported when there are streaming aggregations on streaming DataFrames/DataSets without watermark;;
+}
+/*
+-------------------------------------------
+Batch: 0
+-------------------------------------------
++---+
+|col|
++---+
++---+
+
+-------------------------------------------
+Batch: 1
+-------------------------------------------
++-----+
+|  col|
++-----+
+|first|
++-----+
+
+-------------------------------------------
+Batch: 2
+-------------------------------------------
++------+
+|   col|
++------+
+| first|
+|second|
++------+
+ */
