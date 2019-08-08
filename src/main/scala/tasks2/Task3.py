9227cfe6e165b6022deb4e3f1a8dedd8c40e11af
andrzej.szewczyk <andrzej.szewczyk@accenture.com>
2019-08-08 16:08:01 +0200

    add task1 pyspark

---
+++ src/main/scala/tasks2/Task3.py
@@ -0,0 +1,70 @@
+from pyspark import SparkContext
+from pyspark.sql import *
+import pyspark.sql.functions as f
+
+sc = SparkContext("local", "app")
+sc.setLogLevel('OFF')
+spark = SparkSession(sc)
+
+spark.readStream \
+    .format("socket") \
+    .option("host", "localhost") \
+    .option("port", 9999) \
+    .load() \
+    .agg(f.collect_list(f.col('value')).alias('list')) \
+    .select(f.explode(f.col("list"))) \
+    .writeStream \
+    .format('console') \
+    .outputMode('complete') \
+    .option('checkpointLocation', 'data/checkpoint') \
+    .start() \
+    .awaitTermination()
+
+
+# $ ncat -lk 9999
+#
+# first
+# second
+# third
+
+# -------------------------------------------
+# Batch: 0
+# -------------------------------------------
+# +---+
+# |col|
+# +---+
+# |   |
+# +---+
+#
+# -------------------------------------------
+# Batch: 1
+# -------------------------------------------
+# +-----+
+# |  col|
+# +-----+
+# |     |
+# |first|
+# +-----+
+#
+# -------------------------------------------
+# Batch: 2
+# -------------------------------------------
+# +------+
+# |   col|
+# +------+
+# |      |
+# | first|
+# |second|
+# +------+
+#
+# -------------------------------------------
+# Batch: 3
+# -------------------------------------------
+# +------+
+# |   col|
+# +------+
+# |      |
+# | first|
+# |second|
+# | third|
+# +------+
